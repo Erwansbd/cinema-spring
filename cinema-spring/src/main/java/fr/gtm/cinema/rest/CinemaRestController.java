0c7171dc28e694953a67721f4c6ad841646814be
erwansbd <erwansbd@gmail.com>
2019-11-20 15:57:00 +0100

    Mise en place de l'envoi des mails depuis le webservice REST sous SPRING

--- cinema-spring/src/main/java/fr/gtm/cinema/rest/CinemaRestController.java
+++ cinema-spring/src/main/java/fr/gtm/cinema/rest/CinemaRestController.java
@@ -3,6 +3,8 @@ package fr.gtm.cinema.rest;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.gtm.cinema.dao.ActeurRepository;
 import fr.gtm.cinema.dto.ActeurDTO;
 import fr.gtm.cinema.entities.Acteur;
+import fr.gtm.cinema.util.MailReceptor;
 
 @RestController
 public class CinemaRestController {
 
 	@Autowired
 	ActeurRepository acteurRepository;
+	@Autowired
+	private JavaMailSender mailSender;
 
 	@GetMapping("/acteur/{id}")
 	public ActeurDTO findActeurById(@PathVariable long id) {
@@ -34,6 +39,25 @@ public class CinemaRestController {
 		acteurRepository.save(acteurDTO.toActeur());
 		return "Votre contact : " + acteurDTO.getCivilite() + " " + acteurDTO.getNom() + " " + acteurDTO.getPrenom()
 				+ " a bien été sauvegardé en base de données.";
+	}
+
+	@PostMapping("/mail/send")
+	public String testMail(@RequestBody MailReceptor mailReceptor) {
+		try {
+			new Thread(()->sendMail(mailReceptor)).start();
+		} catch (Exception e) {
+
+		}
+		return "Votre mail a bien été envoyé.";
+	}
+
+	private void sendMail(MailReceptor mailReceptor) {
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(mailReceptor.getEmail());
+		mailMessage.setFrom("Emmanuel.Macron@elysee.gouv.fr");
+		mailMessage.setSubject("Information");
+		mailMessage.setText("Bonjour "+"\u001B[36m"+mailReceptor.getPrenom());
+		mailSender.send(mailMessage);
 	}
 
 }
